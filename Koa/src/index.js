9e99503441ab1230b2bf87213d9298996145639b
grooviiee <ldj7202@gmail.com>
2021-07-20 22:18:09 +0900

    어쩌구

--- Koa/src/index.js
+++ Koa/src/index.js
@@ -1,10 +1,24 @@
 const Koa = require('koa');
-const app = new Koa();
+const Router = require('koa-router');
 
-app.use(ctx => {
-    ctx.body = 'Hello Koa';
+const app = new Koa();
+const router = new Router();
+
+router.get('/', (ctx, next) => {
+    ctx.body = 'HOME';
 });
 
-app.listen(4000, () => {
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(4000, () =>{
     console.log('server is listening to port 4000');
 });
+
+// app.use(ctx => {
+//     ctx.body = 'Hello Koa';
+// });
+
+// app.listen(4000, () => {
+//     console.log('server is listening to port 4000');
+// });
