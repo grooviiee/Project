e07fd9687b63e5a33ebfada30daabdab12d0bfe2
Grooviiee <86159019+grooviiee@users.noreply.github.com>
2022-04-30 14:33:36 +0900

    Create simul.cpp

---
+++ Simulation/simul.cpp
@@ -0,0 +1,64 @@
+#include <cstring>
+#include <sstream>
+#include <vector>
+#include <cmath>
+#include <iostream>
+
+const int ALTITUDE 180;
+const int MAXPOWER 35;
+const int NOISEPOWER -110;
+const int CarrierFreq 38;
+const double PathLossExp 2.3;
+const int NumOfNLOS 4;
+const int AntArraySize 10 * 16;
+const int UserAntSize 4*4;
+
+const int NumRfChain 4;
+const int NumOfUser 10;
+
+const int QosThres 1;
+const double InitialRadius 200;
+const double PositionStepSize 1.4;
+const double BFStepSize 2;
+const double thresholdAlg1 0.05;
+const double thresholdAlg2 0.05;
+
+double calcAchievableRate(){
+    double R;
+
+
+    return R;
+}
+
+double calcSteeringVector(){
+    double a;
+
+    return a;
+}
+
+double calcIntraClusterInt(){
+    dobule I_intra;
+
+    return I_intra;
+}
+
+double calcInterClusterInt(){
+    double I_Inter;
+
+    return I_Inter;
+}
+
+double calcRcvdSignal(){
+    double y;
+    return y;
+}
+
+double calcChannelMatrix(){
+    double H;
+
+    return H; 
+}
+
+int main(){
+    
+}
